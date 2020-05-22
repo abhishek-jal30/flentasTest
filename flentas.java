0c233cd3943e7e4dc8829f6d6cd47cbf95a5d433
unknown <aman@netapp.com>
2020-05-22 23:37:19 +0530

    chnages Added

--- flentas.java
+++ flentas.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
  
-class TestClass {
+class FlentasTestClass {
     
     public static void main (String[] args) throws java.lang.Exception
 	{
